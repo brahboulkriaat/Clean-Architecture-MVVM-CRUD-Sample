1c1c93b22e26270b934110487136163882766c35
Brahim Boulkriaat <bboulkriaat@gmail.com>
2025-09-27 10:59:12 +0200

    LoadingScreen.kt

--- app/src/main/java/com/example/app/ui/detail/DetailsScreen.kt
+++ app/src/main/java/com/example/app/ui/detail/DetailsScreen.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -20,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.app.ui.MainViewModel
+import com.example.app.ui.common.LoadingScreen
 import com.example.domain.model.Post
 import com.example.domain.utill.Result
 
@@ -38,14 +38,11 @@ fun DetailsScreen(
 
         when (result) {
             is Result.Loading -> {
-                //TODO LoadingScreen()
-                Box(
+                LoadingScreen(
                     modifier = Modifier
                         .fillMaxSize()
-                        .padding(it), contentAlignment = Alignment.Center
-                ) {
-                    CircularProgressIndicator()
-                }
+                        .padding(it)
+                )
             }
 
             is Result.Error -> {
